ee4f690204e3dc1f146ed2433a916f62c4c45942
mahsank111 <mahsank111@gmail.com>
2024-02-13 22:51:55 +0530

    Initial Commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createClient } from "@/prismicio";
+import Link from "next/link";
+import { PrismicNextLink } from "@prismicio/next";
+import { label } from "three/examples/jsm/nodes/Nodes.js";
+import { Inder } from "next/font/google";
+import NavBar from "@/components/NavBar";
+
+
+export default async function Header() {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+  return (
+    <header className="top-0 z-50 mx-auto 
+    max-w-7xl md:sticky md:top-4">
+      <NavBar settings={settings} />
+    </header>
+  );
+}
